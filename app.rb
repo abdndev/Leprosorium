1847e9f082bea538cbd8363fde6ecae3066220d5
ABDNdev <igor@abidin.ru>
2019-10-13 11:38:37 +0300

    Add comments

--- app.rb
+++ app.rb
@@ -7,15 +7,21 @@ require 'sqlite3'
 def init_db
 	@db = SQLite3::Database.new 'leprosorium.db'
 	@db.results_as_hash = true
-
 end
 
+# before вызывается каждый раз при перезагрузке
+# любой страницы
 before do
+	#инициализация БД
 	init_db
 end
 
+# вызывается каждый раз при конфигурации приложения:
+# когда изменился код программы и перезагрузилась страница
 configure do
+	# инициалиализация БД
 	init_db
+	# создает таблицу если таблица не существует
 	@db.execute 'CREATE TABLE if not exists "Posts" 
 	(
 		"id" INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -27,11 +33,16 @@ get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
 
+# обработчик get-запроса /new
+# (браузер получает страницу с сервера)
 get '/new' do
   erb :new
 end
 
+# обработчик post-запроса /new
+# (браузер отправляет данные на сервер)
 post '/new' do
+	# получаем переменную из post-запроса
 	content = params[:content]
 
 	erb "You typed: #{content}"
