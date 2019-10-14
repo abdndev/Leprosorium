bef7b19f9ce03142fb56aeec724b0d3d3f1d1770
ABDNdev <igor@abidin.ru>
2019-10-14 19:53:50 +0300

    Create table Comments

--- app.rb
+++ app.rb
@@ -21,12 +21,22 @@ end
 configure do
 	# инициалиализация БД
 	init_db
+
 	# создает таблицу если таблица не существует
 	@db.execute 'CREATE TABLE if not exists "Posts" 
 	(
 		"id" INTEGER PRIMARY KEY AUTOINCREMENT,
 		"created_date" DATE, 
 		"content" TEXT);'
+
+	# создает таблицу если таблица не существует
+	@db.execute 'CREATE TABLE if not exists Comments
+	(
+		"id" INTEGER PRIMARY KEY AUTOINCREMENT,
+		"created_date" DATE, 
+		"content" TEXT,
+		post_id integer 
+	)'
 end
 
 get '/' do
